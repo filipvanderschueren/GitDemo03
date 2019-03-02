6241312780436ca97288fede5f4f7e531b54522e
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-03-02 15:25:15 +0100

    third commit - class added

--- src/gitdemo03/GitDemo03.java
+++ src/gitdemo03/GitDemo03.java
@@ -16,7 +16,10 @@ public class GitDemo03 {
      */
     public static void main(String[] args) {
         System.out.println("first hello...");
-        System.out.println("second hello...");
+        NewClass01 m01 = new NewClass01();
+        m01.setMessage("message from NewClass01....");
+        System.out.println("second hello from :" + m01.getMessage());
+        
         // TODO code application logic here
     }
     
